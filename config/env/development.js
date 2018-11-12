87a296fe3ae99eee08ce1d1073c879bd597045dc
steve-fdn <steve.muerset@iwi.unibe.ch>
2018-11-12 10:28:16 +0100

    added mysql-adapter package and set local env db connection closes #1035

--- config/env/development.js
+++ config/env/development.js
@@ -10,6 +10,32 @@ module.exports = {
 
     custom: {
         baseUrl: 'http://localhost:4200'
-    }
+    },
 
+    datastores: {
+
+        default: {
+
+            /***************************************************************************
+            *                                                                          *
+            * Want to use a different database during development?                     *
+            *                                                                          *
+            * 1. Choose an adapter:                                                    *
+            *    https://sailsjs.com/plugins/databases                                 *
+            *                                                                          *
+            * 2. Install it as a dependency of your Sails app.                         *
+            *    (For example:  npm install sails-mysql --save)                        *
+            *                                                                          *
+            * 3. Then pass it in, along with a connection URL.                         *
+            *    (See https://sailsjs.com/config/datastores for help.)                 *
+            *                                                                          *
+            ***************************************************************************/
+            adapter: 'sails-mysql',
+
+            //your env connection 
+            url: 'mysql://oss:oss@localhost:3306/database',
+        
+          },
+    
+      },
 }
