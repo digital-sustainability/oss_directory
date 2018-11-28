5dd4a9dd46e5095336a527d9398471a9c083bf72
steve-fdn <steve.muerset@iwi.unibe.ch>
2018-11-28 08:54:20 +0100

    created skeleton for backend logging 	helpers: 		write: store log in file 		read: read log from file 		format: format log to make it readable

---
+++ api/helpers/log/format.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+        friendlyName : 'format',
+
+        description : 'formats a log to make it readable',
+
+        inputs : {
+                message : {
+                        type : 'string',
+                        required : true,
+                },
+
+                error : {
+                        type : 'string',
+                        
+                }
+        },
+
+        exits : {
+
+        },
+
+        fn: async function(inputs, exits){
+                
+        }
+};
+
+
